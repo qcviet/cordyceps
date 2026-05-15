875bbd19492b27a90b7597aabe82a72a9af9d160
qcviet <qcviet2012@gmail.com>
2026-05-15 11:24:54 +0700

    Update call process_section

--- templates/introduce-page.php
+++ templates/introduce-page.php
@@ -42,6 +42,19 @@ get_header();
 					]);
 					get_template_part('templates/blocks/about-section', null, $data);
 					break;
+
+				case 'process_section':
+					$data = cordyceps_get_flexible_content_data([
+						'class' => '',
+						'subtitle' => 'subtitle',
+						'title' => 'title',
+						'description' => 'description',
+						'process_items' => 'process_items',
+						'highlighted_content' => 'highlighted_content',
+						'background_highlighted' => 'background_highlighted',
+					]);
+					get_template_part('templates/blocks/process-section', null, $data);
+					break;
 			endswitch;
 		endwhile;
 	}
