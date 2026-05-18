d07099bce023becf7c90ac80579bfac5c8cab22c
qcviet <qcviet2012@gmail.com>
2026-05-18 08:30:20 +0700

    Update call home_about and trust_badges section

--- templates/content/flexible.php
+++ templates/content/flexible.php
@@ -21,6 +21,25 @@ if (have_rows('sections')) {
 				get_template_part('templates/blocks/hero-slider', null, $data);
 				break;
 
+			case 'home_about':
+				$data = cordyceps_get_flexible_content_data([
+					'class' => '',
+					'title' => 'title',
+					'subtitle' => 'subtitle',
+					'description' => 'description',
+					'background_image' => 'background_image',
+					'button_url' => 'button_url',
+				]);
+				get_template_part('templates/blocks/home-about', null, $data);
+				break;
+			case 'trust_badges':
+				$data = cordyceps_get_flexible_content_data([
+					'class' => '',
+					'three_items' => 'three_items',
+					'four_items' => 'four_items',
+				]);
+				get_template_part('templates/blocks/trust-badges', null, $data);
+				break;
 		endswitch;
 	endwhile;
 }
