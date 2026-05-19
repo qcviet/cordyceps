05bc14e40a6a63180f91f037606fbc50592a3253
qcviet <qcviet2012@gmail.com>
2026-05-19 14:44:45 +0700

    Update fix layout product-page

--- templates/product-page.php
+++ templates/product-page.php
@@ -11,40 +11,6 @@
 
 get_header();
 ?>
-<div class="product-page product-page--hero-landing">
-	<?php
-	if (have_rows('product_sections')) {
-		while (have_rows('product_sections')):
-			the_row();
-			$layout = get_row_layout();
-
-			switch ($layout):
-				case 'hero_banner':
-					$data = cordyceps_get_flexible_content_data([
-						'class' => '',
-						'subtitle' => 'subtitle',
-						'title' => 'title',
-						'title_italic' => 'title_italic',
-						'description' => 'description',
-						'icon_items' => 'icon_items',
-						'background_image' => 'background_image',
-					]);
-					get_template_part('templates/blocks/hero-banner', null, $data);
-					break;
-				case 'product_section':
-					$data = cordyceps_get_flexible_content_data([
-						'class' => '',
-						'title' => 'title',
-						'description' => 'description',
-						'category_items' => 'category_items',
-						'category_product' => 'category_product',
-					]);
-					get_template_part('templates/blocks/featured-product', null, $data);
-					break;
-			endswitch;
-		endwhile;
-	}
-	?>
-</div>
+<?php cordyceps_render_product_landing_page(); ?>
 <?php
 get_footer();
